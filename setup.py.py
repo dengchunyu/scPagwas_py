29068b588c2e76fb023564a3a5cae6bb360ece14
Chunyu Deng <dengcyelena@gmail.com>
2024-11-27 13:59:42 +0800

    update setup

---
+++ setup.py.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scPagwas_py",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        # 这里添加你的依赖，例如 "numpy>=1.21.0"
+    ],
+)
